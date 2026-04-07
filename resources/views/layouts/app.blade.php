b722560db7df069395e194970f474ce7005974c9
TheSolver0 <lucfotso0@gmail.com>
2026-04-07 10:26:53 +0100

    retrait de la lignes de vite manifest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     @stack('styles')
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         :root {
             --pad-blue: #1a5490;
